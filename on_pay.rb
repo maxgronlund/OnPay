79cb41bdad6a0ac57be0d1d224dacb1f2cdaa57e
Synthmax <max@synthmax.dk>
2019-02-08 12:49:29 +0100

    Create on_pay.rb

---
+++ on_pay.rb
@@ -0,0 +1,51 @@
+class OnPay
+  require 'openssl'
+  require 'uri'
+  SECRET = 'e88ebc73104651e3c8ee9af666c19b0626c9ecacd7f8f857e3633e355776baad92e67b7faf9b87744f8c6ce4303978ed65b4165f29534118c882c0fd95f52d0c'.freeze
+
+  def self.secret(params = {}, secret)
+    data = on_pay_data(params)
+    encrypt(data, secret)
+  end
+
+  # Remove all keys not starting with 'onpay_'
+  # Sort alphabetic and convert to hash
+  def self.on_pay_data(params)
+    params = params.select { |key, _| key.to_s.include?('onpay_') }
+    params.sort.to_h
+  end
+
+  # Url encode and encode with secret
+  def self.encrypt(data, secret)
+    uri_encoded_data =
+      URI
+      .encode_www_form(data)
+      .downcase
+
+    OpenSSL::HMAC
+      .hexdigest(
+        digest,
+        secret,
+        uri_encoded_data
+      )
+  end
+
+  def self.digest
+    OpenSSL::Digest.new('sha1')
+  end
+
+  # Remove it in production
+  def self.test_data
+    {
+      onpay_gatewayid: '20007895654',
+      onpay_currency: 'DKK',
+      onpay_amount: '12000',
+      onpay_reference: 'AF-847824',
+      onpay_accepturl: 'https://example.com/accept',
+      unrelated_param: 'bla bla bla'
+    }
+  end
+end
+
+# Usage / test
+# OnPay.secret(OnPay.test_data, OnPay::SECRET)
